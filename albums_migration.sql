75fa9f89172a9fbbcb6a075f74a3028325bd8340
George Tudor <Alexander.George.Tudor@gmail.com>
2021-10-04 12:23:51 -0500

    add ALBUM SEEDER and SELECT exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,13 @@
 USE codeup_test_db;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                       artist VARCHAR(65),
-                       name VARCHAR(65),
-                       release_date INT NOT NULL,
-                       sales FLOAT NOT NULL,
-                       genre VARCHAR(65),
+                       artist VARCHAR(200) NOT NULL,
+                       name VARCHAR(200) NOT NULL,
+                       release_date SMALLINT UNSIGNED NOT NULL,
+                       sales FLOAT(6, 2) NOT NULL,
+                       genre VARCHAR(200) NOT NULL,
                        PRIMARY KEY (id)
 );
