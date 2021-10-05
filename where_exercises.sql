80aff21601930cfe1c081e9435212f86505bd451
George Tudor <Alexander.George.Tudor@gmail.com>
2021-10-05 11:56:56 -0500

    complete limit, order, and where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -10,6 +10,11 @@ SELECT * FROM employees
 WHERE last_name LIKE '%q%';
 
 SELECT * FROM employees
+WHERE first_name LIKE 'Irena'
+    OR first_name LIKE 'Vidya'
+    OR first_name LIKE 'Maya';
+
+SELECT * FROM employees
 WHERE gender = 'm' AND (first_name LIKE 'Irena'
    OR first_name LIKE 'Vidya'
    OR first_name LIKE 'Maya');
