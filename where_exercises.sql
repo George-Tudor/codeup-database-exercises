b00d537dcef63cbe8a004ae7a8677819ef340155
George Tudor <Alexander.George.Tudor@gmail.com>
2021-10-04 21:48:38 -0500

    mysql Where Exercises part 2C

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,3 +8,22 @@ WHERE last_name LIKE 'e%';
 
 SELECT * FROM employees
 WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees
+WHERE gender = 'm' AND (first_name LIKE 'Irena'
+   OR first_name LIKE 'Vidya'
+   OR first_name LIKE 'Maya');
+
+SELECT * FROM employees
+WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'e%e';
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
+
+
+
+
